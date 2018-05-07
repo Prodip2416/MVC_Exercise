c083a8a90cd2603c14d3c897e3b64be0855603fb
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-05-07 12:45:23 +0600

    MVC

---
+++ Kudvenket_Tutorial/DisplayNameDisplayFortmate/Models/Employee.partial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DisplayNameDisplayFortmate.Models
+{
+    [MetadataType(typeof(EmployeeMetadata))]
+    public partial class Employee
+    {
+    }
+
+    public class EmployeeMetadata
+    {
+        [DataType(DataType.Url)]
+        [UIHint("OpenNewPage")]
+        public string PersonalWebSite { get; set; } 
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }    
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+        //[DisplayFormat(DataFormatString = "{0:d}")]
+       // [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm:ss tt}")]
+        [DataType(DataType.Time)]
+        public DateTime? HireDate { get; set; }
+        [DisplayFormat(NullDisplayText = "Gender Not Specified")]
+        public string Gender { get; set; }
+        [ScaffoldColumn(false)]
+        public int? Salary { get; set; } 
+    }
+}
