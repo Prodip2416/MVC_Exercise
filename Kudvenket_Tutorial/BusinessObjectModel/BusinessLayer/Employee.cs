1f73f5d4b1a5f442e3d775914ca6a11b533f3afb
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-05-07 12:31:49 +0600

    MVC

---
+++ Kudvenket_Tutorial/BusinessObjectModel/BusinessLayer/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+       [DataType(DataType.DateTime)]
+        public DateTime? DateTime { get; set; }  
+    }
+}
---
+++ Kudvenket_Tutorial/BusinessObjectModel/BusinessObjectModel/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+
+namespace BusinessObjectModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            EmployeeBusinessLayer employeeBusiness= new EmployeeBusinessLayer();
+            List<Employee> employees = employeeBusiness.Employees.ToList();
+            return View(employees);
+        }
+        [HttpGet]
+        [ActionName("Create")]
+        public ActionResult Create_Get()
+        {
+            return View();
+        }
+        [HttpGet]
+        [ActionName("Edit")]
+        public ActionResult Edit(int id)
+        {
+            EmployeeBusinessLayer employees= new EmployeeBusinessLayer();
+            Employee employee = employees.Employees.Single(e => e.EmployeeId == id);
+            return View(employee);
+        }
+        [HttpPost]
+        [ActionName("Edit")]
+        public ActionResult Edit()
+        {
+            if (ModelState.IsValid)
+            {
+                EmployeeBusinessLayer employees= new EmployeeBusinessLayer();
+
+                Employee employee= new Employee();
+                UpdateModel(employee);
+                employees.UpdateEmployee(employee);
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult Create_POst()
+        {
+             //    Employee employee = new Employee();
+            // Retrieve form data using form collection
+            //    employee.Name = name;
+            //    employee.Gender = gender;
+            //    employee.City = city;
+            //    employee.DateTime = datetime;
+
+            if (ModelState.IsValid)
+            {
+                EmployeeBusinessLayer employeeBusinessLayer =
+                new EmployeeBusinessLayer();
+                
+                Employee employee = new Employee();
+                UpdateModel(employee);
+                employeeBusinessLayer.AddEmployee(employee);
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            EmployeeBusinessLayer employeeBusinessLayer= new EmployeeBusinessLayer();
+          
+            employeeBusinessLayer.DeleteEmployee(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
