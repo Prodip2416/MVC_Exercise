dbac14b290eb1eb6b58969e0bae9b88f640aa143
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-05-07 12:28:57 +0600

    MVC

---
+++ Kudvenket_Tutorial/AjaxWithAspNetMVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxWithAspNetMVC.Models;
+
+namespace AjaxWithAspNetMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        SampleDbContext db = new SampleDbContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult GetAllStudent()
+        {
+            List<Student> model = db.Students.ToList();
+            return PartialView("_Student", model);
+        }
+        public PartialViewResult GetTop3Student()
+        {
+            List<Student> model = db.Students.OrderByDescending(x => x.TotalMarks).Take(3).ToList();
+            return PartialView("_Student", model);
+        }
+        public PartialViewResult GetButtom3Student()
+        {
+            List<Student> model = db.Students.OrderBy(x => x.TotalMarks).Take(3).ToList();
+            return PartialView("_Student", model);
+        }
+    }
+}
