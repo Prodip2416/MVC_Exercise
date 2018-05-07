8abeee6f9229169a9cb203dcac7a258a9b9fa4e0
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-05-07 12:52:48 +0600

    MVC

---
+++ Kudvenket_Tutorial/ImplementAutocompleteTextboxFunctionalityMVC/ImplementAutocompleteTextboxFunctionalityMVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ImplementAutocompleteTextboxFunctionalityMVC.Models;
+
+namespace ImplementAutocompleteTextboxFunctionalityMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        SampleDBContext db = new SampleDBContext();
+        public ActionResult Index()
+        {
+            List<Students> student = db.Students.ToList();
+            return View(student);
+        }
+        [HttpPost]
+        public ActionResult Index(string search)
+        {
+            List<Students> student;
+            if (string.IsNullOrEmpty(search))
+            {
+                student = db.Students.ToList();
+            }
+            else
+            {
+                student = db.Students.Where(x => x.Name.StartsWith(search)).ToList();
+            }
+            return View(student);
+        }
+
+        public JsonResult GetStudents(string term)
+        {
+            List<string> student;
+
+            student = db.Students.Where(x => x.Name.StartsWith(term)).Select(y => y.Name).ToList();
+          
+            return Json(student, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Kudvenket_Tutorial/ImplementAutocompleteTextboxFunctionalityMVC/ImplementAutocompleteTextboxFunctionalityMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ImplementAutocompleteTextboxFunctionalityMVC.MvcApplication" Language="C#" %>
