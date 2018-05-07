f88443ca029f35a8e123f0913bea88f966f58fef
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-05-07 12:50:35 +0600

    MVC

---
+++ Kudvenket_Tutorial/HTMLHelperInMVC/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HTMLHelperInMVC.Models;
+
+namespace HTMLHelperInMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        public ActionResult Index()
+        { 
+            return View();
+        }
+
+        //public ActionResult LoadDropDownList()
+        //{
+        //    Company company = new Company("Aushomapto");
+        //    ViewBag.Department = new SelectList(company.Department, "Id", "Name");
+        //    ViewBag.CompanyName = company.CompanyName;
+        //    return View();
+        //}
+        //public ActionResult LoadDropDownList1()
+        //{
+        //    Company company = new Company("Aushomapto");
+
+        //    return View(company);
+        //}
+        [HttpGet]
+        public ActionResult LoadRadioButton()
+        {
+            Company company = new Company();
+
+            return View(company);
+        }
+        [HttpPost]
+        public string LoadRadioButton(Company company)
+        {
+            if (string.IsNullOrEmpty(company.SelectDepartment))
+            {
+                return "You do not have select any department";
+            }
+            else
+            {
+                return "You Have select department with Id = " + company.SelectDepartment;
+            }
+
+        }
+    }
+}
---
+++ Kudvenket_Tutorial/HTMLHelperInMVC/Models/Company.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HTMLHelperInMVC.Models
+{
+    public class Company
+    {
+     //   public string CompanyName { get; set; }
+        public string SelectDepartment { get; set; }
+
+        //public Company(string companyName)
+        //{
+        //    CompanyName = companyName;
+        //}
+        //private string _name;
+        //public Company(string name)
+        //{
+        //    this._name = name;
+        //}
+
+        public List<tblDepartment> Department
+        {
+            get
+            {
+                MVCDropDownList db= new MVCDropDownList();
+                return db.tblDepartments.ToList();
+            }
+        }
+
+        //public string CompanyName
+        //{
+        //    get { return _name; }
+        //    set { _name = value; }
+        //}
+    }
+}
