2c7e9af32dd1bca8c218b889ae5fce6b07b3c160
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-05-07 12:48:08 +0600

    MVC

---
+++ Kudvenket_Tutorial/FirstApplication/FirstApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstApplication.Models;
+
+namespace FirstApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index(int departmentId)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+
+            List<Employee> employees = employeeContext.Employees.Where(e=>e.DepartmentId==departmentId).ToList();
+
+            return View(employees);
+        }
+        public ActionResult Details(int id)
+        {
+            EmployeeContext employeeContext= new EmployeeContext();
+
+            Employee employee = employeeContext.Employees.Single(emp => emp.EmployeeId == id);
+           
+            return View(employee);
+        }
+    }
+}
---
+++ Kudvenket_Tutorial/FirstApplication/FirstApplication/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return typeof(Controller).Assembly.GetName().Version.ToString();
+        }
+
+        public ActionResult Country()
+        {
+           ViewData["Country"]= new List<string>()
+            {
+                "Bangladesh",
+                "India",
+                "USA",
+                "UK"
+            };
+            return View();
+        }
+        public string About()
+        {
+            return "Hello from MVC Application";
+        }
+
+        public string Contact()
+        {
+            return "prodip2416@gmail.com";
+        }
+
+        public string GetDetails(string id)
+        {
+            return "Id = "+ id+" Name = "+Request.QueryString["name"];
+        }
+    }
+}
