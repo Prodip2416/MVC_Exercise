96a22c506a33f9a74eec1fc179e29bf014f20664
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-05-07 12:41:40 +0600

    MVC

---
+++ Kudvenket_Tutorial/DeletingMultipleRowsInMvc/DeletingMultipleRowsInMvc/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DeletingMultipleRowsInMvc.Models;
+
+namespace DeletingMultipleRowsInMvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        SampleDBContext db= new SampleDBContext();
+        public ActionResult Index()
+        {       
+            return View(db.Employees.ToList());
+        }
+        public ActionResult Delete(IEnumerable<int> deleteIdEmployee )
+        {
+            db.Employees.Where(x => deleteIdEmployee.Contains(x.EmployeeId)).ToList().ForEach(db.Employees.d);
+          
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
