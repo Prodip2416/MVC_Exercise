142cb8978940ebe48b6f0b5bf86dd7b04b7e4707
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-05-07 13:01:32 +0600

    MVC

---
+++ Kudvenket_Tutorial/InsertUpdateDeleteUsingEntityFramework/Models/Department.partial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InsertUpdateDeleteUsingEntityFramework.Models
+{
+    [MetadataType(typeof(DepartmentMetadata))]
+    public partial class Department
+    {
+
+    }
+    public class DepartmentMetadata
+    {
+        [Display(Name = "Department Name")]
+        public string Name { get; set; }    
+    }
+}
---
+++ Kudvenket_Tutorial/InsertUpdateDeleteUsingEntityFramework/Models/DepartmentTotal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InsertUpdateDeleteUsingEntityFramework.Models
+{
+    public class DepartmentTotal
+    {
+        public string Name { get; set; }
+        public int Total { get; set; }
+
+
+    }
+}
