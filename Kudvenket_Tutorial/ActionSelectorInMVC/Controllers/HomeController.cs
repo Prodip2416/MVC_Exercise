b841b9b2a59fd3fd6669dca8dc019bae1c0e3d2c
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-05-07 12:22:00 +0600

    Add files via upload

---
+++ Kudvenket_Tutorial/ActionSelectorInMVC/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionSelectorInMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+     
+        [ActionName("List")]
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
